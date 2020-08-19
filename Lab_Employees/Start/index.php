04e0a1f1c0edea763c30bc4fe1741226016a86b8
Brave <brave_chen@mail.chungyo.net>
2020-08-19 17:58:09 +0800

    add class notes and practice

--- Lab_Employees/Start/index.php
+++ Lab_Employees/Start/index.php
@@ -1,12 +1,52 @@
+<?php
+    require_once("config.php"); // 引用檔案（最好副檔名都為php）
+	// $dbhost = 'localhost';
+	// $dbuser = 'root';
+	// $dbpass = 'root';
+	// $dbname = 'directory';
+    $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, 8889); // 建立與mysql資料庫的連接
+    // var_dump($link);
+    // $sqlCommand = "SELECT * FROM employee"; // sql command v1
+    $sqlCommand = "SELECT * , (SELECT COUNT(*) FROM employee WHERE managerId = e.id) AS reporter FROM employee AS e"; // sql command v2
+    // var_dump($link);
+    $result = mysqli_query($link, $sqlCommand); // 對資料庫下sql指令
+    // var_dump($result);
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Lab</title>
 <meta name="viewport" content="width=device-width,initial-scale=1" />
-
+<script src="scripts/jquery-1.9.1.min.js"></script>
+<script src="scripts/jquery.mobile-1.3.2.min.js"></script>
+<link rel="stylesheet" href="scripts/jquery.mobile-1.3.2.min.css" />
+<link rel="stylesheet" href="styles.css" />
 </head>
 <body>
+<div data-role="page" data-theme="c">
 
+<div data-role="header">
+	<h1>Employee Details</h1>
+</div>
+
+<div data-role="content">
+	<ul data-role="listview" data-filter="true">
+        <?php while($oneRow = mysqli_fetch_assoc($result)) { ?>
+            <li>
+                <a href="employeeDetails.php?id=<?php echo $oneRow["id"] ?>"> 
+                    <img src="images/<?php echo $oneRow["picture"] ?>">
+                    <h4><?php echo $oneRow["firstName"] . "&nbsp;" . $oneRow["lastName"] ?></h4>
+                    <p><?php echo $oneRow["title"]?></p> <span class="ui-li-count"><?php echo $oneRow["reporter"]?></span>
+                </a>
+            </li>
+        <?php } ?>
+	</ul>
+</div>
+
+</div>
+<?php
+    mysqli_close($link);
+?>
 </body>
 </html>
--- Lab_MySQL_basic_1_start.php
+++ Lab_MySQL_basic_1_start.php
@@ -5,24 +5,27 @@ header("content-type:text/html; charset=utf-8");
 
 
 // 1. 連接資料庫伺服器
-// $link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
-// $result = mysqli_query($link, "set names utf8");
+$link = @mysqli_connect("localhost", "root", "root", "class", 8889) or die(mysqli_connect_error());
+// var_dump($link);
+$result = mysqli_query($link, "set names utf8");
 // mysqli_select_db($link, "class");
+// var_dump($result);
 
 // 2. 執行 SQL 敘述
-// $commandText = "select * from students";
-// $result = mysqli_query($link, $commandText);
+$commandText = "select * from students";
+$result = mysqli_query($link, $commandText); // Returns FALSE on failure. For successful SELECT, SHOW, DESCRIBE or EXPLAIN queries mysqli_query() will return a mysqli_result object.
+// var_dump($result);
 
 // 3. 處理查詢結果
-// while ($row = mysqli_fetch_assoc($result))
-// {
-//   echo "ID：{$row['cID']}<br>";
-//   echo "Name：{$row['cName']}<br>";
-//   echo "<HR>";
-// }
+while ($row = mysqli_fetch_assoc($result))
+{
+  echo "ID：{$row['cID']}<br>";
+  echo "Name：{$row['cName']}<br>";
+  echo "<HR>";
+}
 
 // 4. 結束連線
-// mysqli_close($link);
+mysqli_close($link);
 
 echo "<br />-- Done --";
 ?>
